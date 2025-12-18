cb71eb4bdbe726b9bdaa3d1c889c42f929c7aa58
VanniMaceria <andreabucci123@gmail.com>
2025-12-18 18:01:55 +0100

    Red phase test_video_non_aperto

--- test/test_frame_provider.py
+++ test/test_frame_provider.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch, MagicMock
 import numpy as np
 import cv2
+from src.exceptions.VideoOpenError import VideoOpenError
 from src.frame_provider import FrameProvider
 
 
@@ -25,3 +26,18 @@ class TestFrameProvider(unittest.TestCase):
         # --- ASSERT ---
         self.assertIsInstance(result, np.ndarray)  # Verifica che il frame letto sia un n-dimensional array nunmpy
         self.assertTrue(np.array_equal(result, fake_frame)) # Verifica che il frame sia proprio fake_frame
+
+        # Ricordati di importare l'eccezione all'inizio del file!
+        # from src.exceptions import VideoOpenError
+
+    @patch.object(cv2, 'VideoCapture')
+    def test_init_raises_exception_when_video_cannot_be_opened(self, video_capture: MagicMock):
+        # --- ARRANGE ---
+        mock_frame_taker = MagicMock()
+        mock_frame_taker.isOpened.return_value = False
+        video_capture.return_value = mock_frame_taker
+
+        # --- ACT & ASSERT ---
+        self.assertRaises(VideoOpenError, FrameProvider, "bad_path.mp4")    # Verifica che init() lanci l'eccezione
+
+

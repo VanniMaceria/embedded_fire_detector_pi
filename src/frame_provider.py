f3c4689f799ba15480bc77ec8589e0d6c4e2d9cc
VanniMaceria <andreabucci123@gmail.com>
2025-12-18 18:38:25 +0100

    Green phase controlla se il video è ripartito

--- src/frame_provider.py
+++ src/frame_provider.py
@@ -16,9 +16,16 @@ class FrameProvider:
     def get_frame(self) -> np.ndarray:
         """Cattura un frame e lo restituisce come array NumPy."""
         result = self.capturer.read()   # Restituisce l'esito (bool) e la matrice.
-        #outcome = result[0]    # Restituisce falso quando il video è finito o non è riuscito a leggere il frame. Quando è così si fa ripartire il video per dare l'effetto loop
+        read_success = result[0]    # Restituisce falso quando il video è finito o non è riuscito a leggere il frame. Quando è così si fa ripartire il video per dare l'effetto loop
         frame = result[1]
 
+        if not read_success:    # Il video deve ripartire
+            self.capturer.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            result = self.capturer.read()   # Inizia a leggere da capo
+            read_success = result[0]
+            frame = result[1]
 
+            if not read_success:
+                raise VideoOpenError("Errore nell'apertura del video")
 
         return frame

5c42abcac21c691b91ee3942fa16b46caba123bf
VanniMaceria <andreabucci123@gmail.com>
2025-12-20 17:15:40 +0100

    Green phase test_connects_to_broker_on_init

--- src/alert_notifier.py
+++ src/alert_notifier.py
@@ -1,12 +1,19 @@
+import paho.mqtt.client as mqtt
+
 class AlertNotifier:
     """Gestisce la comunicazione con il server esterno."""
 
     def __init__(self, broker: str, topic: str):
-        pass
+        self.broker = broker
+        self.topic = topic
+
+        self.is_alert_active = False    # Stato interno: serve ancora per non mandare 1000 messaggi al secondo
+        self.client = mqtt.Client()
+        self.client.connect(self.broker)    # Prova a connettersi quando si chiama il costruttore
 
     def send_alert(self, timestamp: str, confidence: float) -> bool:
         """
-        Invia una messaggio MQTT via RestAPI
+        Invia una messaggio MQTT
         Restituisce True se l'invio ha successo.
         """
         pass
